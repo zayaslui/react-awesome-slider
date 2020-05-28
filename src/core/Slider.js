dbb70b3fce767e1b821706bb988b68e3a6583af5
Luis Zayas <zayaslui@gmail.com>
2020-05-28 13:05:24 -0400

    update

---
+++ src/core/Slider.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import RangeSlider from 'react-bootstrap-range-slider';
+
+const periodos = [ 2012 , 2013 , 2014, 2015]
+
+const defaultProps = {
+	min:0,
+	max:10
+}
+
+function Slider(props){
+const [ value, setValue ] = useState(0); 
+
+  return (
+    <RangeSlider
+    	{...props}
+		value={value}
+		tooltipLabel={ (label) => {
+			console.log(label)
+			return periodos[label]
+		}}
+		onChange={changeEvent => {
+			let numero =  parseInt(changeEvent.target.value)
+			setValue(numero);
+			setTimeout( () => {
+				props.funcion(numero)
+			},2000)
+		}}
+    />
+  );  
+}
+
+Slider.defaultProps = defaultProps;
+export default Slider;
